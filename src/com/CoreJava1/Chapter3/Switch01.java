c3f2b36e1ca5a7902b6b6751800ac02d2bebfbf0
liyang206 <yangAOk2025@outlook.com>
2026-03-01 09:25:40 +0800

    "Switch的用法，注意标签的类型匹配"

---
+++ src/com/CoreJava1/Chapter3/Switch01.java
@@ -0,0 +1,49 @@
+package com.CoreJava1.Chapter3;
+
+import java.util.Scanner;
+
+public class Switch01 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int seasonCode = scan.nextInt();
+
+        String seasonName = switch (seasonCode) {
+            case 1 -> "Spring";
+            case 2 -> "Summer";
+            case 3 -> "Fall";
+            case 4 -> "Winter";
+            default -> "???";
+        };
+
+        System.out.println(seasonName);
+
+
+        //多个标签
+        int sumLetters = switch (seasonName){
+            case "Spring", "Summer", "Fall" -> 6;
+            case "Winter" -> 4;
+            default -> -1;
+        };
+
+        System.out.println(sumLetters);
+
+
+        //使用枚举常量
+        enum Size {
+            SMALL, MEDIUM, LARGE;
+        };
+
+        Size itemSize = Size.valueOf(scan.next());
+
+        String label = switch (itemSize){
+            case SMALL -> "S";
+            case MEDIUM -> "M";
+            case LARGE -> "L";
+            default -> "?";
+        };
+
+        System.out.println(label);
+
+    }
+}
