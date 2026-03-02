f7b53b9f42dffa7112763f28ea79cb6f97308f76
liyang206 <yangAOk2025@outlook.com>
2026-03-02 08:17:28 +0800

    "部分常见StringAPI列举"

---
+++ src/com/CoreJava1/Chapter3/StringAPI.java
@@ -0,0 +1,18 @@
+package com.CoreJava1.Chapter3;
+
+public class StringAPI {
+    public static void main(String[] args) {
+        /*
+        部分常见API
+        int codePointAt(int index);
+        boolean isEmpty();
+        boolean isBlank();
+        boolean startsWith();
+        boolean endsWith();
+        使用newString替换原字符串中与oldString匹配的子串：
+        String replace(CharSequence oldString, CharSequence newString)
+        String subString(int beginIndex, int endIndex)
+        .............更多详见API文档
+         */
+    }
+}
